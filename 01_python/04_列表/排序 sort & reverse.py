e327b7717c71358fc7be1c20a0a7d33fbdeed310
jinkin1985@aliyun.com <jinkin1985@aliyun.com>
2026-03-15 13:53:36 +0000

    33

---
+++ 01_python/04_列表/排序 sort & reverse.py
@@ -0,0 +1,6 @@
+l1 = [3,2,4,5,1]
+l1.sort() # 升序排序
+print(l1) 
+l1.reverse()
+print(l1) # 降序排序
+
---
+++ 01_python/04_列表/排序 sorted (函数).py
@@ -0,0 +1,5 @@
+l1 = [2,3,5,1,4,6]
+l2 = sorted(l1)
+print(l2)
+l3 = sorted(l1, reverse=True)
+print(l3) 
