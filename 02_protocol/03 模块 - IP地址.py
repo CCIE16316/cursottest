0d5c82b4cea575783eec8be0d04ec37b43448f71
jinkin1985@aliyun.com <jinkin1985@aliyun.com>
2026-03-15 02:27:36 +0000

    22

---
+++ 02_protocol/03 模块 - IP地址.py
@@ -0,0 +1,12 @@
+import ipaddress
+
+net1 = ipaddress.ip_network('192.168.2.0/24')
+for ip in net1:
+    print (str(ip))
+    # print(type(ip))
+    # 这里的ip不是字符串，是Class，所以要转换成字符串。
+print(net1.num_addresses)
+# 统计数量
+print(ipaddress.ip_address('192.168.2.2') in net1)
+# 判断地址是否在范围内
+
