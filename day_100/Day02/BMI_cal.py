7964d733319d88d04a618fa7716b3cf453bb1a40
jinkin1985@aliyun.com <jinkin1985@aliyun.com>
2026-03-17 03:53:09 +0000

    44

--- day_100/Day02/BMI_cal.py
+++ day_100/Day02/BMI_cal.py
@@ -4,5 +4,5 @@ weight = 84
 # Write your code here.
 # Calculate the bmi using weight and height.
 bmi = weight / (height **2)
-
 print(bmi)
+print(round(bmi,2)) # 四舍五入到第二位小数点
---
+++ day_100/Day02/number_manipulation.py
@@ -0,0 +1,8 @@
+score = 0
+score += 1 # 其实就是score + 1 =
+# score -= 1
+# score *= 1
+# score /= 1
+print(type(score))
+print("Your score is: " + str(score))
+
