1f970f1fb834a8698fe23768c329f9a573c5c7c5
jinkin1985@aliyun.com <jinkin1985@aliyun.com>
2026-03-19 13:10:48 +0000

    44

--- day_100/Day04/game.py
+++ day_100/Day04/game.py
@@ -1,17 +1,19 @@
 import random
 
-result = ["Rock","Scissors","Paper"]
+result = ["Rock","Paper","Scissors"]
 choice_people = int(input("What do you choose? type 0 for Rock, 1 for Paper or 2 for Scissors."))
 choice_computer = random.randint(0,2)
 
 if choice_people == choice_computer:
-    print(f"{result[choice_people]} vs {result[choice_computer]}, 平局")
+    print(f"{result[choice_people]} vs {result[choice_computer]}, It's a draw.")
+elif choice_people >=3 or choice_people <0:
+    print("Wrong Number")
 else:
     if choice_people == 1 and choice_computer == 0:
-        print(f"{result[choice_people]} vs. {result[choice_computer]}, You Win!")
+        print(f"People Choice:{result[choice_people]} vs. Computer Choice:{result[choice_computer]}, You Win!")
     elif choice_people == 2 and choice_computer == 1:
-        print(f"{result[choice_people]} vs. {result[choice_computer]}, You Win!")
+        print(f"People Choice:{result[choice_people]} vs. Computer Choice:{result[choice_computer]}, You Win!")
     elif choice_people == 0 and choice_computer == 2:
-        print(f"{result[choice_people]} vs. {result[choice_computer]}, You Win!")
+        print(f"People Choice:{result[choice_people]} vs. Computer Choice:{result[choice_computer]}, You Win!")
     else:
-        print(f"{result[choice_people]} vs. {result[choice_computer]}, You Lost!")
+        print(f"People Choice:{result[choice_people]} vs. Computer Choice:{result[choice_computer]}, You Lost!")
