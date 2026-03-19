c17a3bd8030e8f03318f0da0a61abb31790642d1
jinkin1985@aliyun.com <jinkin1985@aliyun.com>
2026-03-19 06:59:13 +0000

    333

--- day_100/Day03/treasure_island.py
+++ day_100/Day03/treasure_island.py
@@ -1,3 +1,10 @@
+print('''     _ _       _    _       _       
+    | (_)_ __ | | _(_)_ __ | | __   
+ _  | | | '_ \| |/ / | '_ \| |/ /   
+| |_| | | | | |   <| | | | |   <    
+ \___/|_|_| |_|_|\_\_|_| |_|_|\_\   
+                                     ''')
+
 print(f"Welcome to Treasure Island.\nYour missions is to find the treasure.")
 left_right = input("left or right? ")
 if left_right == "right":
