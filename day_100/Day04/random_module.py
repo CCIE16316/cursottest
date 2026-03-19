672db966de225b216d2f94244e61ece84133edf6
jinkin1985@aliyun.com <jinkin1985@aliyun.com>
2026-03-19 07:56:43 +0000

    555

---
+++ day_100/Day04/random_module.py
@@ -0,0 +1,7 @@
+import random
+import my_module
+
+print(my_module.my_favourite_numbermy_favourite_number) # 这里其实就是调用了一个py，其实也是一个module
+
+random_integer = random.randint(1,10)
+print(random_integer)
