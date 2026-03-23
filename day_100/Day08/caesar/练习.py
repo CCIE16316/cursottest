d235ab4feb43d0607278fcc9e90d2316ca6e81e1
jinkin1985@aliyun.com <jinkin1985@aliyun.com>
2026-03-23 02:10:02 +0000

    444

--- day_100/Day08/caesar/练习.py
+++ day_100/Day08/caesar/练习.py
@@ -1,4 +1,6 @@
-from main import alphabet
+import string
+from unittest import TestResult
+alphabet = list(string.ascii_letters)
 
 def all_in_one(orginal_text,shift,encode_or_decode):
     output_text = ""
@@ -7,7 +9,17 @@ def all_in_one(orginal_text,shift,encode_or_decode):
     for letter in orginal_text: # e.g. CCC
         # C
         position = alphabet.index(letter) + shift
-        alphabet[position].append(output_text)
+        position %= len(alphabet)
+        output_text += alphabet[position] # 这里目标对象是str，所以不能用append，只能用+= 
     print(output_text)
 
-all_in_one("ccc",2,"encode")
+all_in_one("eee",2,"decode")
+
+
+should_continue = True
+while should_continue:
+    method = input("Encode or Decode? \n").lower()
+    choice = input("Yes or No\n").lower()
+    if choice == "no":
+        should_continue = False
+
